122f4c689d9452fd39dd930b4ed17736b166b892
Luke Durell <jlukedurell@gmail.com>
2020-10-26 15:37:08 -0500

    reorganize data

---
+++ R/eml.R
@@ -0,0 +1,12 @@
+#' Eagle Mountain Lake Profile Data Sets
+#'
+#' Data from the Eagle Mountain Lake
+#'
+#' @docType data
+#'
+#' @usage data(eml)
+#'
+#' @format An object of class \code{"data.frame"}
+#' @examples
+#' data(eml)
+"eml"
