4095f88b12de21105ca09740346f51fd976d1f8a
Luke Durell <jlukedurell@gmail.com>
2020-10-26 15:20:33 -0500

    eml data set init.

---
+++ R/eml-data.R
@@ -0,0 +1,15 @@
+#' Eagle Mountain Lake Profile Data
+#'
+#' Data from Eagle Mountain Lake
+#'
+#' @docType data
+#'
+#' @usage data(eml)
+#'
+#' @format An object of class \code{'data.frame'}
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(eml)
+"eml"
