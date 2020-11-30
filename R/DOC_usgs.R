4c79a5189682f931bf4c2926d4d2b2d98d341bd8
Luke Durell <jlukedurell@gmail.com>
2020-11-30 17:34:51 -0600

    add DOC_usgs and DOC_baseflow

---
+++ R/DOC_usgs.R
@@ -0,0 +1,32 @@
+#' DOC_usgs
+#'
+#' Profile data from Eagle Mountain Lake containing depth profiles of
+#' water quality variables. Data was collected beginning on April 25, 2019.
+#' It was collected at 21 depths, every 0.5 meters from 0.0 (surface) to 10.0 meters.
+#' A vertical profile measurement was taken once every two hours.
+#' All times are in UTC.
+#'
+#' @docType data
+#'
+#' @usage data(DOC_usgs)
+#'
+#' @format An object of class \code{"data.frame"} with 35532 rows and 7 variables:
+#' \describe{
+#'   \item{Date-Time}{Date and two-hour time period measurement was taken}
+#'   \item{Depth}{Profile Depth, measured in meters}
+#'   \item{Temp}{Water Temperature, measured in degrees Celsius}
+#'   \item{DO}{Dissolved Oxygen, measured in mg/L}
+#'   \item{DOsat}{Dissolved Oxygen Saturation, measured as the percentage of DO relative to what the concentration would be in equilibrium with the atmosphere, calcualted as actual/expected}
+#'   \item{pH}{pH, measured as standard pH from 0 (basic) to 14 (acidic)}
+#'   \item{Cond}{Conductivity in water, measured in \eqn{\mu}S/cm (micro Siemens per centimeter)}
+#' }
+#'
+#' @source Professor Thad Scott from the Department of Biology at Baylor University provided this data.
+#' Tarrant Regional Water District (TRWD) provided funding for the first year of data collection, and Eagle Mountain Lake is owned and operated by TRWD.
+#' This lake is used to supply water to municipalities within the Dallas/Fort Worth area.
+#'
+#'
+#'
+#' @examples
+#' plot(DOC_usgs$Latitude, DOC_usgs$Longitude)
+"DOC_usgs"
