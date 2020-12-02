81d5c536bf695924790bb354c931cb3167de11c9
Luke Durell <jlukedurell@gmail.com>
2020-12-02 11:08:30 -0600

    build out LIFT documentation

--- R/LIFT.R
+++ R/LIFT.R
@@ -1,13 +1,26 @@
-#' LIFT
+#' LIFT Wastewater Treatment Data
 #'
-#' Water treatment facility data.
+#' The LIFT (Leaders Innovation Forum for Technology) data set
+#' gives water quality measurements at numerous locations
+#' in a water treatment facility. The facility is divided into
+#' nine zones. Multiple probes are used measure dissolved oxygen,
+#' ammonia, and nitrate.
+#'
 #'
 #' @docType data
 #'
-#' @usage data(LIFT)
+#' @usage
+#' LIFT
+#' data(LIFT) # loads into working environment
 #'
-#' @format An object of class \code{"data.frame"} with 25908 rows and 15 variables measuring ammonia at different locations
-#'
+#' @format LIFT is an object of class \code{"data.frame"} with 25908
+#' rows and 16 columns.
 #'
 #' @examples
+#' ammonia <- LIFT[,11]
+#' length(ammonia)
+#' ts.plot(ammonia)
+#' ts.plot(ammonia[24000:25908])
+#' hist(ammonia)
+#' hist(ammonia,breaks="FD")
 "LIFT"
